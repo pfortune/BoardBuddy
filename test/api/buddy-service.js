02d50dbb46aa52bdde3cd900b2c952c8646d29ca
Peter Fortune <pfortune@gmail.com>
2024-03-04 01:25:27 +0000

    Added Buddy Service handlers for Games and Locations

--- test/api/buddy-service.js
+++ test/api/buddy-service.js
@@ -28,4 +28,69 @@ export const buddyService = {
         const res = await axios.get(`${this.buddyUrl}/api/users`);
         return res.data;
     },
+
+    async createGame(game) {
+        const res = await axios.post(`${this.buddyUrl}/api/games`, game);
+        return res.data;
+    },
+
+    async getGame(id) {
+        const res = await axios.get(`${this.buddyUrl}/api/games/${id}`);
+        return res.data;
+    },
+
+    async deleteGame(id) {
+        const res = await axios.delete(`${this.buddyUrl}/api/games/${id}`);
+        return res.data;
+    },
+
+    async deleteAllGames() {
+        const res = await axios.delete(`${this.buddyUrl}/api/games`);
+        return res.data;
+    },
+
+    async getAllGames() {
+        const res = await axios.get(`${this.buddyUrl}/api/games`);
+        return res.data;
+    },
+
+    async createLocation(location) {
+        const res = await axios.post(`${this.buddyUrl}/api/locations`, location);
+        return res.data;
+    },
+
+    async getLocation(id) {
+        const res = await axios.get(`${this.buddyUrl}/api/locations/${id}`);
+        return res.data;
+    },
+
+    async getLocationsByUser(id) {
+        const res = await axios.get(`${this.buddyUrl}/api/users/${id}/locations`);
+        return res.data;
+    },
+
+    async getLocationsByCategory(id) {
+        const res = await axios.get(`${this.buddyUrl}/api/categories/${id}/locations`);
+        return res.data;
+    },
+    
+    async getGamesByLocation(id) {
+        const res = await axios.get(`${this.buddyUrl}/api/locations/${id}/games`);
+        return res.data;
+    },
+
+    async deleteLocation(id) {
+        const res = await axios.delete(`${this.buddyUrl}/api/locations/${id}`);
+        return res.data;
+    },
+
+    async deleteAllLocations() {
+        const res = await axios.delete(`${this.buddyUrl}/api/locations`);
+        return res.data;
+    },
+
+    async getAllLocations() {
+        const res = await axios.get(`${this.buddyUrl}/api/locations`);
+        return res.data;
+    },
 };
