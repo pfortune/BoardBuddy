aac44d0c3bd89fd21e82a029a90135be5167221a
Peter Fortune <pfortune@gmail.com>
2024-03-08 20:01:29 +0000

    Fixed search

--- src/utils/handlebar-helper.js
+++ src/utils/handlebar-helper.js
@@ -3,3 +3,5 @@ import Handlebars from "handlebars";
 Handlebars.registerHelper("isEqual", function(arg1, arg2, options) {
   return (arg1 === arg2) ? options.fn(this) : options.inverse(this);
 });
+
+Handlebars.registerHelper("toTitleCase", (str) => str.toLowerCase().split(" ").map((word) => (word.charAt(0).toUpperCase() + word.slice(1))).join(" "));
